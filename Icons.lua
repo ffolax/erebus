8f99f3ff072842c07ccd4392a3f305ba45b33059
ffolax <truly.jjtatum@gmail.com>
2026-07-06 03:49:58 -0500

    Define icons for Window and Sidebar
    Added icon definitions for Window and Sidebar.

--- Icons.lua
+++ Icons.lua
@@ -1,1 +1,18 @@
+return {
 
+    Window = {
+        Close = "rbxassetid://...",
+        Minimize = "rbxassetid://...",
+        Maximize = "rbxassetid://..."
+    },
+
+    Sidebar = {
+        Home = "rbxassetid://...",
+        Visuals = "rbxassetid://...",
+        Combat = "rbxassetid://...",
+        Vehicle = "rbxassetid://...",
+        Misc = "rbxassetid://...",
+        Settings = "rbxassetid://..."
+    }
+
+}
