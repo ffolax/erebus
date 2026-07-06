2029502d1c5acc9d8fd7cf21ae69edf18a0d2b05
ffolax <truly.jjtatum@gmail.com>
2026-07-06 03:07:54 -0500

    Update and rename main.lua to Main.lua

---
+++ Main.lua
@@ -0,0 +1,5 @@
+local Context = loadstring(game:HttpGet("https://raw.githubusercontent.com/USER/REPO/main/Context.lua"))()
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/USER/REPO/main/UI.lua"))()
+
+Context.Init()
+UI.Init(Context)
--- main.lua
+++
@@ -1,8 +0,0 @@
-local uiScript = "https://raw.githubusercontent.com/ffolax/erebus/main/ui.lua"
-local success, result = pcall(loadstring(game:HttpGet(uiScript)))
-
-if success then
-    result()
-else
-    print("Failed to load UI: " .. tostring(result))
-end
