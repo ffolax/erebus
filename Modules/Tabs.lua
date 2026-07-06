f3afde4ec21f7f82a3e7204693c95dc074500188
ffolax <truly.jjtatum@gmail.com>
2026-07-06 03:13:17 -0500

    Implement tab system with Main and Settings tabs

--- Modules/Tabs.lua
+++ Modules/Tabs.lua
@@ -1,1 +1,26 @@
+return function(Context, UI)
 
+    local Tabs = {}
+
+    Tabs.Main = {
+        OnOpen = function()
+            print("Main tab opened")
+
+            UI:CreateButton("Test Button", function()
+                print("Clicked!")
+            end)
+        end
+    }
+
+    Tabs.Settings = {
+        OnOpen = function()
+            print("Settings tab opened")
+        end
+    }
+
+    for name, data in pairs(Tabs) do
+        Context:RegisterTab(name, data)
+    end
+
+    Context:SetActiveTab("Main")
+end
