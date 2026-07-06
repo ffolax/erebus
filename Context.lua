ca65d53670aef467de14132fd400dbe65c113efd
ffolax <truly.jjtatum@gmail.com>
2026-07-06 03:12:07 -0500

    Implement Context module with tab management

--- Context.lua
+++ Context.lua
@@ -1,1 +1,25 @@
+local Context = {}
 
+Context.State = {
+    Tabs = {},
+    ActiveTab = nil,
+    UI = nil
+}
+
+function Context:Init()
+    print("[Context] Initialized")
+end
+
+function Context:RegisterTab(name, data)
+    self.State.Tabs[name] = data
+end
+
+function Context:SetActiveTab(name)
+    self.State.ActiveTab = name
+
+    if self.State.Tabs[name] and self.State.Tabs[name].OnOpen then
+        self.State.Tabs[name].OnOpen()
+    end
+end
+
+return Context
