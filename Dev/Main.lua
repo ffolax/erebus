e05bf0ca29e604b35e4f749bdf2a932d5bc583d3
ffolax <truly.jjtatum@gmail.com>
2026-07-06 20:55:04 -0500

    load tab element fix

--- Dev/Main.lua
+++ Dev/Main.lua
@@ -40,6 +40,18 @@ local Vehicle = loadstring(game:HttpGet(BASE .. "Tabs/Vehicle.lua"))()
 local Visuals = loadstring(game:HttpGet(BASE .. "Tabs/Visuals.lua"))()
 local Misc = loadstring(game:HttpGet(BASE .. "Tabs/Misc.lua"))()
 
+local Tabs = {
+    Home,
+    Player,
+    Vehicle,
+    Visuals,
+    Misc
+}
+
+for _,Tab in ipairs(Tabs) do
+    Tab.Load(Context)
+end
+
 UI:RegisterTab("Home", Home)
 UI:RegisterTab("Player", Player)
 UI:RegisterTab("Vehicle", Vehicle)
