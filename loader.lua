7d939dfc2db13190c03218040ed8f49bed05ac64
ffolax <truly.jjtatum@gmail.com>
2026-07-06 19:10:50 -0500

    dev check

--- loader.lua
+++ loader.lua
@@ -1,3 +1,15 @@
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/ffolax/erebus/main/Release/Main.lua"
-))()
+local p = game:GetService("Players")
+
+if p.UserId == 11232890519 then
+
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/ffolax/erebus/main/dev/Main.lua"
+    ))()
+
+else
+
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/ffolax/erebus/main/Release/Main.lua"
+    ))()
+
+end
