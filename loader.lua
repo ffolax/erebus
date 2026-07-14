40cb9c4d2596e15099696d9d26f54322cbedbaa6
ffolax <truly.jjtatum@gmail.com>
2026-07-13 22:47:39 -0500

    changed dev user id

--- loader.lua
+++ loader.lua
@@ -8,7 +8,7 @@ repeat
 
 until p
 
-if p.UserId == 11253251281 then
+if p.UserId == 11284190585 then
 
     loadstring(game:HttpGet(
         "https://raw.githubusercontent.com/ffolax/erebus/main/Dev/Main.lua"
